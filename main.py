a21afd944576e38005b3b3828f092f79e670cd7a
Advaith126 <advaithpk@outlook.com>
2025-02-22 10:41:28 -0800

    Final Test

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
-# Create the main window
 root = tk.Tk()
-
-# Set the title of the window
-root.title("My Tkinter Window")
-
-# Set the size of the window
-root.geometry("400x300")
-
-# Run the application
-root.mainloop()
-
-print("Hello World.")
+root.title("My GUI App")    
