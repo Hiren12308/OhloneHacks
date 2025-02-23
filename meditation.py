f18b50f2c89ec8f039d34760d0e58d26402216a0
Taran Nulu <taran.nulu@gmail.com>
2025-02-22 17:48:19 -0800

    Change meditation bar into a square

--- meditation.py
+++ meditation.py
@@ -1,21 +1,32 @@
 import customtkinter as ctk
-import tkinter as tk
 
 stop_countdown = False
 
-def countdown(count, message, on_complete=None):
+def countdown(count, message, current_bar = "left", on_complete=None):
     global stop_countdown
-
     if stop_countdown:
         stop_countdown = False
+        left_bar.stop()
         return
 
     label.configure(text=count)
     message_label.configure(text=message)
 
-    if count >= 0:
-        window.after(1000, countdown, count - 1, message, on_complete)
-        meditation_progress_bar.start()
+    if current_bar == "left":
+        bottom_bar.stop()
+        left_bar.start()
+    if current_bar == "top":
+        left_bar.stop()
+        top_bar.start()
+    if current_bar == "right":
+        top_bar.stop()
+        right_bar.start()
+    if current_bar == "bottom":
+        right_bar.stop()
+        bottom_bar.start()
+
+    if count >= 1:
+        root.after(1000, countdown, count - 1, message, current_bar, on_complete)
     else:
         if on_complete:
             on_complete()
@@ -23,36 +34,58 @@ def countdown(count, message, on_complete=None):
 def run_countdowns(sequence, index=0):
     global stop_countdown
     stop_countdown = False
+
     if index < len(sequence):
-        count, message = sequence[index]
-        countdown(count, message, lambda: run_countdowns(sequence, index + 1))
+        count, message, current_bar = sequence[index]
+        countdown(count, message, current_bar, lambda: run_countdowns(sequence, index + 1))
 
 def terminate_meditation():
     global stop_countdown
     stop_countdown = True
 
 sequence = [
-    (4, "Breathe in..."),
-    (4, "Hold..."),
-    (4, "Breathe out..."),
-    (4, "Hold...")
+    (4, "Breathe in...", "left"),
+    (4, "Hold...", "top"),
+    (4, "Breathe out...", "right"),
+    (4, "Hold...", "bottom")
 ]
 
-window = ctk.CTk()
+root = ctk.CTk()
 
-label = ctk.CTkLabel(window, text="")
+screenwidth = root.winfo_screenwidth()
+screenheight = root.winfo_screenheight()
+
+root.geometry(f"{int(screenwidth * 0.8)}x{int(screenheight * 0.8)}")
+
+label = ctk.CTkLabel(root, text="")
 label.place(x=35, y=15)
 
-message_label = ctk.CTkLabel(window, text="")
+message_label = ctk.CTkLabel(root, text="")
 message_label.place(x=35, y=50)
 
-start_meditation = ctk.CTkButton(window, text="Start Meditation", command=lambda: run_countdowns(sequence))
-stop_meditation = ctk.CTkButton(window, text="Stop Meditation", command=lambda: terminate_meditation())
+start_meditation = ctk.CTkButton(root, text="Start Meditation", command=lambda: run_countdowns(sequence) )
 start_meditation.grid(row=0, column=0, padx=35, pady=150)
+
+stop_meditation = ctk.CTkButton(root, text="Stop Meditation", command=terminate_meditation)
 stop_meditation.grid(row=1, column=0, padx=35, pady=10)
 
-meditation_progress_bar = ctk.CTkProgressBar(window, orientation = "horizontal", indeterminate_speed=0.5)
-meditation_progress_bar.set(0)
-meditation_progress_bar.place(x=35, y=75)
+bar_speed = 0.262
+length = 500
 
-window.mainloop()
+left_bar = ctk.CTkProgressBar(root, orientation="vertical", determinate_speed=bar_speed, height=length)
+left_bar.set(0)
+left_bar.place(x=35, y=75)
+
+top_bar = ctk.CTkProgressBar(root, orientation="horizontal", determinate_speed=bar_speed, width=length)
+top_bar.set(0)
+top_bar.place(x=35, y=75)
+
+right_bar = ctk.CTkProgressBar(root, orientation="vertical", determinate_speed=bar_speed, height=length)
+right_bar.set(0)
+right_bar.place(x=35+length, y=75)
+
+bottom_bar = ctk.CTkProgressBar(root, orientation="horizontal", determinate_speed=bar_speed, width=length)
+bottom_bar.set(0)
+bottom_bar.place(x=35, y=75+length)
+
+root.mainloop()
