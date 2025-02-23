98e65c7c2d12ed43728fa305ef3898b1c5bfa491
Taran Nulu <taran.nulu@gmail.com>
2025-02-23 09:53:00 -0800

    Render in a single function

--- meditation.py
+++ meditation.py
@@ -6,6 +6,11 @@ root = ctk.CTk()
 adjusted_screenwidth = (root.winfo_screenwidth()) * 0.8
 adjusted_screenheight = (root.winfo_screenheight()) * 0.8
 
+INHALE = "Breathe in..."
+INHALE_HOLD = "Inhale hold..."
+EXHALE = "Breathe out..."
+EXHALE_HOLD = "Exhale hold..."
+
 # Flag for stop button
 stop = False
 
@@ -20,7 +25,9 @@ frame_duration_milliseconds = 1000 // frames_per_second
 
 box_stage_duration_seconds = 4
 box_stage_duration_milliseconds = box_stage_duration_seconds * 1000
-radius_increment = (max_radius - min_radius) / (box_stage_duration_seconds * frames_per_second)
+
+frames_per_box_stage = box_stage_duration_seconds * frames_per_second
+radius_increment_size = (max_radius - min_radius) / (box_stage_duration_seconds * frames_per_second)
 
 def render_circle_stage(stage_name, radius, radius_increment, duration_milliseconds):
     if stop: return
@@ -31,6 +38,27 @@ def render_circle_stage(stage_name, radius, radius_increment, duration_milliseco
     canvas.coords(circle, center_x - radius, center_y - radius, center_x + radius, center_y + radius)
     radius += radius_increment
     root.after(frame_duration_milliseconds, render_circle_stage, stage_name, radius, radius_increment, duration_milliseconds)
+
+def render(frame, stage_name, radius, radius_increment):
+    if stop: return
+    if frame % frames_per_box_stage == 0:
+        if stage_name == INHALE:
+            stage_name = INHALE_HOLD
+            radius_increment = 0
+        elif stage_name == INHALE_HOLD:
+            stage_name = EXHALE
+            radius_increment = -radius_increment_size
+        elif stage_name == EXHALE:
+            stage_name = EXHALE_HOLD
+            radius_increment = 0
+        elif stage_name == EXHALE_HOLD:
+            stage_name = INHALE
+            radius_increment = radius_increment_size
+    message_label.configure(text = stage_name)
+    canvas.coords(circle, center_x - radius, center_y - radius, center_x + radius, center_y + radius)
+    frame += 1
+    radius += radius_increment
+    root.after(frame_duration_milliseconds, render, frame, stage_name, radius, radius_increment)
 
 def render_circle():
     if stop: return
@@ -55,14 +83,12 @@ def render_circle():
     stage_start_milliseconds += box_stage_duration_milliseconds
     root.after(stage_start_milliseconds, render_circle)
 
+
 def start_meditation():
     global stop
     stop = False
 
-    render_circle()
-    # if index < len(sequence):
-    #     count, message = sequence[index]
-    #     countdown(count, message, lambda: run_countdowns(sequence, index + 1))
+    render(1, INHALE, min_radius, radius_increment_size)
 
 def terminate_meditation():
     global stop
